72896f69db83bcde4d12dc187ce0085e85617c55
ErikBoesen <me@erikboesen.com>
2020-09-07 15:03:49 -0600

    Remove yaledining dependency

--- app/tasks.py
+++ app/tasks.py
@@ -5,12 +5,12 @@ import os
 import requests
 import re
 from bs4 import BeautifulSoup
-from yaledining import YaleDining as FastTrackAPI
 
 
 @celery.task
 def scrape():
-    # Old FastTrack-based dining API, which still provides non-menu data
-    ft = FastTrackAPI()
-    locations = ft.locations()
+    # Reach out to old FastTrack-based dining API,
+    # which still provides non-menu data
+    FASTTRACK_ROOT =
+    r = requests.get(FASTTRACK_ROOT + 'locations.cfm'
     for
