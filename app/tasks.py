68df095813545cc833777a7a41c7c0bdc36a3ddf
ErikBoesen <me@erikboesen.com>
2020-09-07 15:06:32 -0600

    Add request logic to FT API

--- app/tasks.py
+++ app/tasks.py
@@ -11,6 +11,16 @@ from bs4 import BeautifulSoup
 def scrape():
     # Reach out to old FastTrack-based dining API,
     # which still provides non-menu data
-    FASTTRACK_ROOT =
-    r = requests.get(FASTTRACK_ROOT + 'locations.cfm'
-    for
+    FASTTRACK_ROOT = 'https://www.yaledining.org/fasttrack/'
+    params = {
+        'version': self.API_VERSION,
+    }
+    r = requests.get(FASTTRACK_ROOT + 'locations.cfm', params=params)
+    data = request.json()
+    # Restructure data into a list of dictionaries for easier manipulation
+    data = [
+        {data['COLUMNS'][index]: entry[index] for index in range(len(entry))}
+        for entry in data['DATA']
+    ]
+    Location.query.delete()
+    for raw in data:
