84651efce9e84415d9e95902f33fe9b2d11a621c
ErikBoesen <me@erikboesen.com>
2020-09-08 18:24:46 -0600

    Standardize naming and sleep

--- app/driver.py
+++ app/driver.py
@@ -13,10 +13,10 @@ driver.implicitly_wait(10)
 ###################################
 # Functions for getting UI elements
 
-def get_header():
+def get_header_text():
     return driver.find_element_by_class_name('label-main-caption').text
 
-def get_subheader():
+def get_subheader_text():
     return driver.find_element_by_class_name('label-sub-caption').text
 
 def get_tabs():
@@ -30,16 +30,22 @@ def get_courses():
     print('Found %d courses this time.' % len(courses))
     return courses
 
-def get_ingredients_and_nutrition():
+def get_ingredients_and_nutrition_buttons():
     return driver.find_elements_by_css_selector('.v-button.v-widget.multiline.v-button-multiline.selection.v-button-selection.icon-align-right.v-button-icon-align-right.v-has-width')
-
-def click_back():
-    time.sleep(0.3)
-    driver.find_element_by_css_selector('.button-navigation--previous .v-button').click()
-    time.sleep(0.3)
 
 def get_item_nutrition_buttons():
     return driver.find_elements_by_css_selector('.v-button.nutrition')
+
+def click_back():
+    sleep()
+    driver.find_element_by_css_selector('.button-navigation--previous .v-button').click()
+    sleep()
+
+######################
+# Other util functions
+
+def sleep():
+    time.sleep(0.8)
 
 ################################
 # Parsing process functions
@@ -49,19 +55,20 @@ def scan_to_start():
     while True:
         panels = driver.find_elements_by_class_name('v-panel-content')
         if len(panels) == 1:
+            # The only panel is the no menus error message
             break
         previous_date_button = driver.find_element_by_class_name('button-date-selection--previous')
         previous_date_button.click()
-        time.sleep(1)
+        sleep()
 
 def parse_right():
     # Cycle through dates, collecting data
     while True:
-        time.sleep(0.5)
+        sleep()
         next_date_button = driver.find_element_by_class_name('button-date-selection--next')
         next_date_button.click()
 
-        time.sleep(0.5)
+        sleep()
 
         date = driver.find_element_by_css_selector('.v-label.v-widget.sub-title.v-label-sub-title.v-has-width').text
         print('Parsing date %s...' % date)
@@ -69,7 +76,7 @@ def parse_right():
         panels = driver.find_elements_by_class_name('v-panel-content')
         if len(panels) == 1:
             break
-        time.sleep(0.5)
+        sleep()
         tabs = get_tabs()
         print('Found %d tabs on this page.' % len(tabs))
         has_tabs = (len(tabs) > 0)
@@ -79,9 +86,9 @@ def parse_right():
         while tabs_processed < len(tabs):
             # TODO: remove repetition
             tabs = get_tabs()
-            time.sleep(0.3)
+            sleep()
             tabs[tabs_processed].click()
-            time.sleep(1)
+            sleep()
             meal_name = tabs[tabs_processed].text
 
             courses = get_courses()
@@ -89,12 +96,12 @@ def parse_right():
             while courses_processed < len(courses):
                 courses = get_courses()
                 courses[courses_processed].click()
-                time.sleep(0.3)
+                sleep()
 
                 # Grab and parse Ingredients page
-                ingredients_and_nutrition = get_ingredients_and_nutrition()
-                ingredients_and_nutrition[0].click()
-                time.sleep(0.5)
+                in_buttons = get_ingredients_and_nutrition_buttons()
+                in_buttons[0].click()
+                sleep()
                 rows = driver.find_elements_by_css_selector('.v-panel-content.v-scrollable')[-1].find_elements_by_xpath('//div[@class="v-slot"]')
                 rows_processed = 0
                 print('Found %d rows.' % len(rows))
@@ -102,9 +109,9 @@ def parse_right():
 
                 click_back()
                 # Do again to reattach to the list
-                ingredients_and_nutrition = get_ingredients_and_nutrition()
-                ingredients_and_nutrition[1].click()
-                time.sleep(0.5)
+                in_buttons = get_ingredients_and_nutrition_buttons()
+                in_buttons[1].click()
+                sleep()
                 # TODO: parse nutrition facts page too
                 items = get_item_nutrition_buttons()
                 if items:
@@ -113,22 +120,22 @@ def parse_right():
                         # TODO: stop this from running twice on the first go. And same with other such constructs in this file.
                         items = get_item_nutrition_buttons()
                         items[items_processed].click()
-                        time.sleep(1)
+                        sleep()
 
                         # TODO: parse these pages too
                         click_back()
 
                         items_processed += 1
                 click_back()  # to Ingredients/Nutrition Facts Selection pane
-                time.sleep(0.5)
+                sleep()
                 click_back()  # to main page/meal
-                time.sleep(0.6)
+                sleep()
                 courses_processed += 1
 
             tabs_processed += 1
 
 
-college = get_header()
+college = get_header_text()
 print('Parsing ' + college)
 scan_to_start()
 parse_right()
