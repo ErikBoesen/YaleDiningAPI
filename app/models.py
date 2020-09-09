a18869e380e3af33c1f0519db68479557e002a83
ErikBoesen <me@erikboesen.com>
2020-09-09 01:48:18 -0600

    Add Meal and Item models

--- app/models.py
+++ app/models.py
@@ -24,3 +24,31 @@ class Manager(db.Model):
 
     location_id = db.Column(db.Integer, db.ForeignKey('locations.id'))
     location = db.relationship('Location', back_populates='managers')
+
+class Meal(db.Model):
+    __tablename__ = 'meals'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String)
+
+    vegetarian = db.Column(db.Boolean, default=False)
+    vegan = db.Column(db.Boolean, default=False)
+
+    alcohol = db.Column(db.Boolean, default=False)
+    nuts = db.Column(db.Boolean, default=False)
+    shellfish = db.Column(db.Boolean, default=False)
+    peanuts = db.Column(db.Boolean, default=False)
+    dairy = db.Column(db.Boolean, default=False)
+    egg = db.Column(db.Boolean, default=False)
+    pork = db.Column(db.Boolean, default=False)
+    seafood = db.Column(db.Boolean, default=False)
+    soy = db.Column(db.Boolean, default=False)
+    wheat = db.Column(db.Boolean, default=False)
+    gluten = db.Column(db.Boolean, default=False)
+    coconut = db.Column(db.Boolean, default=False)
