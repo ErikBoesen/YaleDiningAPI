7782362fb8b425daf9bb4accd38207c2bc16950d
ErikBoesen <me@erikboesen.com>
2020-09-09 02:02:08 -0600

    Add Nutrition model

--- app/models.py
+++ app/models.py
@@ -31,6 +31,7 @@ class Meal(db.Model):
     name = db.Column(db.String, nullable=False)
     date = db.Column(db.Date, nullable=False)
 
+
 class Item(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -52,3 +53,41 @@ class Item(db.Model):
     wheat = db.Column(db.Boolean, default=False)
     gluten = db.Column(db.Boolean, default=False)
     coconut = db.Column(db.Boolean, default=False)
+
+class Nutrition(db.Model):
+    __tablename__ = 'nutrition'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    portion_size = db.Column(db.String)
+    calories = db.Column(db.String)
+
+    total_fat = db.Column(db.String)
+    saturated_fat = db.Column(db.String)
+    trans_fat = db.Column(db.String)
+    cholesterol = db.Column(db.String)
+    sodium = db.Column(db.String)
+    total_carbohydrate = db.Column(db.String)
+    dietary_fiber = db.Column(db.String)
+    total_sugars = db.Column(db.String)
+    protein = db.Column(db.String)
+    vitamin_d = db.Column(db.String)
+    vitamin_a = db.Column(db.String)
+    vitamin_c = db.Column(db.String)
+    calcium = db.Column(db.String)
+    iron = db.Column(db.String)
+    potassium = db.Column(db.String)
+
+    total_fat_pdv = db.Column(db.Integer)
+    saturated_fat_pdv = db.Column(db.Integer)
+    trans_fat_pdv = db.Column(db.Integer)
+    cholesterol_pdv = db.Column(db.Integer)
+    sodium_pdv = db.Column(db.Integer)
+    total_carbohydrate_pdv = db.Column(db.Integer)
+    dietary_fiber_pdv = db.Column(db.Integer)
+    total_sugars_pdv = db.Column(db.Integer)
+    protein_pdv = db.Column(db.Integer)
+    vitamin_d_pdv = db.Column(db.Integer)
+    vitamin_a_pdv = db.Column(db.Integer)
+    vitamin_c_pdv = db.Column(db.Integer)
+    calcium_pdv = db.Column(db.Integer)
+    iron_pdv = db.Column(db.Integer)
+    potassium_pdv = db.Column(db.Integer)
