3f572e1c1121a2c62035381d714fdb5325bf654a
ErikBoesen <me@erikboesen.com>
2020-09-11 16:50:26 -0600

    Send location_id with managers

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class Location(db.Model):
 class Manager(db.Model):
     __tablename__ = 'managers'
     _to_expand = ()
-    _to_exclude = ('location_id', 'location')
+    _to_exclude = ('location')
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     email = db.Column(db.String)
