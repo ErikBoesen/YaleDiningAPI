7477ee564acf3ad001b6fa73f9e4dac3c41b87ef
ErikBoesen <me@erikboesen.com>
2020-09-14 17:49:48 -0600

    Refactor out get_last_day function

--- app/tasks.py
+++ app/tasks.py
@@ -429,6 +429,18 @@ def parse_right(college):
     return True
 
 
+def get_last_day(college):
+    location = Location.query.filter_by(name=college).first()
+    last_meal = Meal.query.filter_by(location_id=location.id).order_by(Meal.date.desc()).first()
+    last_day = last_meal.date if last_meal else None
+    if college in menus:
+        last_cached_day = menus[college][-1]['date']
+        # Make lexicographic comparison
+        if last_day is None or last_cached_day > last_day:
+            last_day = last_cached_day
+    return last_day
+
+
 def parse(location_id):
     finished = False
     while not finished:
@@ -440,22 +452,13 @@ def parse(location_id):
         try:
             # If there's already some days in the list, then go to the next day.
             # Otherwise, go all the way to the start.
-            # TODO: in theory, if we didn't run the scraper for a really long time, this could take us back to a time where there's no data. Hopefully we'll run often enough that this won't happen, but it would be good to be sure.
-            location = Location.query.filter_by(name=college).first()
-            last_meal = Meal.query.filter_by(location_id=location.id).order_by(Meal.date.desc()).first()
-            last_day = None
-            if last_meal is not None:
-                last_day = last_meal.date
-            if last_meal is None and college not in menus:
-                seek_start()
-            else:
-                # TODO: clean up conditional mess
-                if college not in menus:
-                    last_cached_day = menus[college][-1]['date']
-                    # Make lexicographic comparison
-                    if last_day is not None and last_cached_day > last_day:
-                        last_day = last_cached_day
+            # TODO: in theory, if we didn't run the scraper for a really long time, this could take us back to a time where there's no data.
+            # Hopefully we'll run often enough that this won't happen, but it would be good to be sure.
+            last_day = get_last_day(college)
+            if last_day:
                 seek_date(day_after(last_day))
+            else:
+                seek_start()
             finished = parse_right(college)
         except (ElementClickInterceptedException, ElementNotInteractableException, IndexError) as e:
             print('Squashing error...')
