bbffe0417c182547a156e26e3b39c4c1c41ad5d9
ErikBoesen <me@erikboesen.com>
2021-03-09 20:22:00 -0500

    Add status_color

--- app/mail.py
+++ app/mail.py
@@ -17,11 +17,22 @@ def send_mail(subject, html, recipients):
         mail.send(msg)
 
 
+def status_color(days_left):
+    if days_left <= 3:
+        return 'red'
+    if days_left <= 5:
+        return 'orange'
+    if days_left <= 7:
+        return 'yellow'
+    return 'green'
+
+
 def send_scraper_report(stats):
     with app.app_context():
         html = render_template('mail/scraper_report.html', stats=stats,
-                               DATE_FMT=DATE_FMT, TIME_FMT=TIME_FMT, DATETIME_FMT=DATETIME_FMT)
-    send_mail(subject='YaleDine Scraper Report ' + stats['end_time'].strftime(DATETIME_FMT),
+                               DATE_FMT=DATE_FMT, TIME_FMT=TIME_FMT, DATETIME_FMT=DATETIME_FMT,
+                               status_color=status_color)
+    send_mail(subject='YaleDine Scraper Report ' + stats['end_time'].strftime(DATE_FMT),
               html=html,
               recipients=app.config['ADMIN_EMAILS'])
 
